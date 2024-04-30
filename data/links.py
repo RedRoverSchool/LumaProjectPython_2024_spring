3964f16249881917c5cecb9831a614ade708e0f6
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-30 15:29:53 +0300

    add test  TC_006.006.002, add link to  what's new page

--- data/links.py
+++ data/links.py
@@ -2,4 +2,4 @@
 WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women.html'
 TOPS_WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women/tops-women.html'
 BOTTOMS_WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women/bottoms-women.html'
-
+WHATS_NEW_PAGE_LINK = 'https://magento.softwaretestingboard.com/what-is-new.html'
--- tests/test_whats_new.py
+++ tests/test_whats_new.py
@@ -4,7 +4,7 @@ from selene import browser
 from pages.main_page import MainPage
 
 
-@allure.suite("Testing What's New Page")
+@allure.suite("US_006.006 | Testing What's New Page")
 class TestWhatsNew:
     @allure.title("TC_006.006.001 | Test visibility of What's New link on the home page")
     def test_whats_new_link_visibility(self, browser_management):
@@ -15,3 +15,20 @@ class TestWhatsNew:
             page.is_menu_present()
         with allure.step("Assert there is the What's New link in the menu"):
             page.is_whats_new_link_present()
+
+    @allure.title("TC_006.006.002 | Check redirection to What's New page by clicking a link")
+    def test_redirection_to_whats_new_page(self, browser_management):
+        with allure.step("Open home page"):
+            page = MainPage(browser=browser)
+            page.open_page()
+        with allure.step("Find What's New link in  the menu"):
+            link = page.find_whats_new_link()
+        with allure.step("Click on What's New link"):
+            link.click()
+        page = WhatsNewPage(browser=browser)
+        with allure.step("Assert current url == What's New Page url"):
+            assert page.check_current_url() == WHATS_NEW_PAGE_LINK
+        with allure.step("Find header"):
+            header = page.is_header_present()
+        with allure.step("Assert header contains text \'What's New\'"):
+            page.is_element_text_correct(header, "What's New")
