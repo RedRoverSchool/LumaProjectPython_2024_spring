e087e8b3747a555d397000d875a3e055f031fb84
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-30 15:43:00 +0300

    TC_006.006.002 | What's new page > Check redirection to What's New page by clicking a link

--- data/links.py
+++ data/links.py
@@ -1,4 +1,4 @@
-
+MAIN_PAGE_LINK = 'https://magento.softwaretestingboard.com/'
 WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women.html'
 TOPS_WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women/tops-women.html'
 BOTTOMS_WOMEN_PAGE_LINK = 'https://magento.softwaretestingboard.com/women/bottoms-women.html'
