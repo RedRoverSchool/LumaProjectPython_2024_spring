77a439652dfbfd164555b39c13ee5fe48c945a4e
omalanova <oyuna_eles@mail.ru>
2024-05-01 23:41:37 +0800

    TC_005.001.001_1 deleted WITH

--- tests/test_minicart.py
+++ tests/test_minicart.py
@@ -1,5 +1,5 @@
 import allure
-from selene import browser, be
+from selene import browser
 from selene.support.shared.jquery_style import s
 
 from pages.main_page import MainPage
@@ -9,19 +9,14 @@ from pages.locators import ProductLocators as PL
 class TestMiniCart:
     def test_minicart_has_link(self, browser_management):
 
-        with allure.step("Open home page"):
-            page = MainPage(browser=browser)
-            page.open_page()
-        with allure.step("Put Radiant Tee in the cart"):
-            s(PL.RADIANT_TEE_SIZE).click()
-            s(PL.RADIANT_TEE_COLOR).click()
-            s(PL.ADD_TO_CART_BUTTON_FROM_MAINPAGE).click()
-        with allure.step("Find and click the cart icon"):
-            page.is_find_cart_icon_present()
-            page.find_cart_icon().click()
-        with allure.step("The minicart is open"):
-            page.is_minicart_present()
-        with (allure.step("Minicart has link View and Edit Cart")):
-            page.is_minicart_have_link()
+        page = MainPage(browser=browser)
+        page.open_page()
+        s(PL.RADIANT_TEE_SIZE).click()
+        s(PL.RADIANT_TEE_COLOR).click()
+        s(PL.ADD_TO_CART_BUTTON_FROM_MAINPAGE).click()
+        page.is_find_cart_icon_present()
+        page.find_cart_icon().click()
+        page.is_minicart_present()
+        page.is_minicart_have_link()
 
 
