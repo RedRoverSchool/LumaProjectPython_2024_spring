7a15982294ad52384122601936ae03d42defec25
Liliana <lilianakoric@gmail.com>
2024-04-30 22:41:41 -0700

    TC_005.001.002| Main page > Cart> User redirected inside the cart

--- pages/add_to_cartLK.py
+++ pages/add_to_cartLK.py
@@ -9,6 +9,7 @@ SIZE_BTN = ("xpath", "//*[@id='option-label-size-143-item-167']")
 COLOR_BTN = ("xpath", "//*[@id='option-label-color-93-item-59']")
 ADD_TO_CATT_BTN = ("xpath", "//*[@id='product-addtocart-button']/span")
 #SUCCESS_MESSAGE = ("xpath", "//span[contains(text(), 'You added Breathe-Easy Tank to your shopping cart.')]")
+
 CART_ICON = ("//*[@class='action showcart']")
 VIEW_AND_EDIT_CART_BLUE_TEXT = ("xpath", "//*[@id='minicart-content-wrapper']/div[2]/div[5]/div/a/span")
 a = '//*[@class="action showcart"]'
@@ -23,8 +24,10 @@ def open_main_page():
 def click_title_item():
     s("//a[@title='Breathe-Easy Tank']").click()
 
+
 def click_size_button():
     s("//*[@id='option-label-size-143-item-167']").click()
+
 
 def click_color_button():
     s("//*[@id='option-label-color-93-item-59']").click()
@@ -51,6 +54,7 @@ def click_cart_icon2():
 
 # def assert_view_and_edit_cart_blue_text():
 #     s(by.xpath(VIEW_AND_EDIT_CART_BLUE_TEXT)).should(be.present)
+
 
 def assert_view_and_edit_cart_blue_text():
     # s(by.xpath(VIEW_AND_EDIT_CART_BLUE_TEXT[1])).should(be.present)
