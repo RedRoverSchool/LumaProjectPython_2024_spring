ad051549ef86c7b540598db06b2892038533f42d
Sviatlana-Kot <sve3363@gmail.com>
2024-05-03 13:28:10 +0300

    TC_014.001.004 | Wish list > Visibility, Clickability > Check button "Update Wish List" is clickable

--- tests/test_wish_list.py
+++ tests/test_wish_list.py
@@ -1,9 +1,11 @@
+import allure
 from selene import browser
 
 from pages import wish_list
 from pages.whats_new_page import WhatsNewPage
 
 
+@allure.link("https://trello.com/c/kVWLOEl5")
 def test_button_update_clickable(login):
     WhatsNewPage(browser=browser).add_item_to_wish_list()
     wish_list.visit()
