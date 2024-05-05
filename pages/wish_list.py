1c068ed8e0c0456f81b256265d6d5dc11f9909ea
Lokankara <poliyakpavlo@gmail.com>
2024-05-05 22:47:07 +0300

    TC_014.003.003| Wish list > Removing and Edit Items

--- pages/wish_list.py
+++ pages/wish_list.py
@@ -1,4 +1,4 @@
-from selene import browser, be, have
+from selene import browser, be, have, command
 from selene.support.shared.jquery_style import s, ss
 
 url = "https://magento.softwaretestingboard.com/wishlist/"
@@ -18,3 +18,28 @@ def should_be_clickable():
 
 def url_should_contain(param):
     browser.should(have.url_containing(param))
+
+
+def get_products():
+    return ss('.product-item-info')
+
+
+def verify_trash_bin_icon_present():
+    items = get_products()
+    size = len(items)
+    count = 0
+    for i in range(size):
+        items[i].perform(command.js.scroll_into_view)
+        items[i].hover()
+        trash_icon = items[i].s('.btn-remove.action.delete')
+        trash_icon.should(be.visible)
+        trash_icon.should(be.present)
+        count += 1
+    assert count == size
+
+
+def remove_item_from_wish_list(product_index):
+    product = get_products()[product_index]
+    trash_bin_icon = product.s('.btn-remove.action.delete')
+    trash_bin_icon.click()
+
