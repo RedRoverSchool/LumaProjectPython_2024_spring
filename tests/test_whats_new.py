c398eb6d0df5c8de531cb12b84958ebe71109edf
daryarosado <darya.rosado@gmail.com>
2024-05-07 12:53:58 -0400

    Conflict resolve

--- tests/test_whats_new.py
+++ tests/test_whats_new.py
@@ -51,3 +51,19 @@ class TestWhatsNew:
 
 
 
+
+
+    @allure.link("https://trello.com/c/bCZOe2Tp/97-tc006006003-whats-new-page-check-lumas-latest-list-visibility")
+    @allure.title("TC_006.006.003 | Check Luma`s latest list visibility")
+    def test_lumas_latest_list_visibility(self):
+        page = MainPage(browser=browser)
+        page.open_page()
+        page.find_whats_new_link().click()
+        page = WhatsNewPage(browser=browser)
+        page.is_lumas_latest_present()
+        item_number = page.get_number_of_lumas_latest()
+        assert item_number == 4
+        assert page.are_men_and_women_items_present() is True
+
+
+
