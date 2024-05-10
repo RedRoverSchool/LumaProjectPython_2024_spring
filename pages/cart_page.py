9a9d7971ff8d7ac70af922351511e75943e61cb7
omalanova <oyuna_eles@mail.ru>
2024-05-10 09:36:50 +0800

    changes were made to the code after the review

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -15,16 +15,24 @@ class CartPage:
     def open_page(self):
         self.browser.open(CART_LINK)
 
+    def find_cart_icon(self):
+        return s(HL.CART_ICON)
+
+    def is_cart_icon_clickable(self):
+        return self.find_cart_icon().should(be.clickable)
+
     def find_qty(self):
         return s(Cart.QTY)
 
     def is_qty_present(self):
-        return s(Cart.QTY).should(be.present)
+        return self.find_qty().should(be.present)
 
     def set_value_of_qty(self, value):
         self.is_qty_present()
         self.find_qty().clear()
         self.find_qty().send_keys(value)
+
+    def click_update_shopping_cart_button(self):
         self.is_update_shopping_cart_button_present()
         self.find_update_shopping_cart_button().click()
 
@@ -32,10 +40,13 @@ class CartPage:
         return s(Cart.UPDATE_SHOPPING_CART_BUTTON)
 
     def is_update_shopping_cart_button_present(self):
-        return s(Cart.UPDATE_SHOPPING_CART_BUTTON).should(be.present)
+        return self.find_update_shopping_cart_button().should(be.present)
 
     def find_counter_number(self):
         return s(HL.MINICART_COUNTER)
 
     def is_counter_number_present(self):
         return self.find_counter_number().should(be.present)
+
+    def is_counter_number_visible(self):
+        return self.find_counter_number().should(be.visible)
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,21 +1,21 @@
-import time
-
 import allure
 
-from selene import browser, have
+from selene import browser, have, be
 from pages.main_page import MainPage
 from pages.cart_page import CartPage
-from pages.locators import ProductLocators as PL
 
 @allure.title("Test Checking the quantity of item in the cart is able to change")
 class TestCart:
     def test_the_quantity_of_item_in_the_cart_is_able_to_change(self):
+        value_of_qty = '2'
         page_cart = CartPage(browser=browser)
         page = MainPage(browser=browser)
+
         page.open_page()
-        page.add_item_to_cart(PL.RADIANT_TEE_SIZE, PL.RADIANT_TEE_COLOR, PL.ADD_TO_CART_BUTTON_FROM_MAINPAGE)
+        page.add_item_to_cart('[option-label="XS"]', '[option-label="Orange"]', 'form[data-product-sku="WS12"] button')
+
         page_cart.open_page()
-        page_cart.set_value_of_qty('2')
-        time.sleep(3)
-        page_cart.is_counter_number_present()
-        page_cart.find_counter_number().should(have.text('2'))
+        page_cart.set_value_of_qty(value_of_qty)
+        page_cart.click_update_shopping_cart_button()
+        page_cart.find_counter_number().should(be.visible)
+        page_cart.find_counter_number().should(have.text(value_of_qty))
