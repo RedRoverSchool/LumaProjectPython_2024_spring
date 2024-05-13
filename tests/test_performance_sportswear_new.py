8770cf9342351881403cfac65ee4deb7789dd254
Cruz06 <Olarut72@gmail.com>
2024-05-13 22:21:57 +0300

    TC_006.008.002_004 | What's new > Performance plus comm

--- tests/test_performance_sportswear_new.py
+++ tests/test_performance_sportswear_new.py
@@ -15,6 +15,7 @@ def test_check_count_of_products(login):
     assert performans_new_page.items_count() == 5
 
 
+@allure.link("https://trello.com/c/9B5bXFEP")
 def test_006_008_001_visibility_of_price_photo_name():
     browser.open(LINK_SPORT)
     nr_of_items_on_page = len(ss(BaseLocators.PRODUCT_ITEM_IN_CATALOG))
@@ -23,6 +24,7 @@ def test_006_008_001_visibility_of_price_photo_name():
     ss(BaseLocators.PRODUCT_IMAGE).should(have.size(nr_of_items_on_page))
 
 
+@allure.link("https://trello.com/c/cmwZ3A6P")
 def test_006_008_002_add_to_cart_from_catalog_without_color_and_size():
     browser.open(LINK_LOGIN)
     s(LoginLocators.FIELD_NAME).type("ahahah1@gmail.com")
@@ -34,18 +36,20 @@ def test_006_008_002_add_to_cart_from_catalog_without_color_and_size():
     s(PerformanceSportswear.SUCCESS_MESSAGE).should(have.no.text(PerformanceSportswear.TEXT_SUCCESS_MESSAGE))
 
 
+@allure.link("https://trello.com/c/cmwZ3A6P")
 def test_006_008_002_add_to_cart_from_catalog_without_color_and_size_with_hover():
+    # another variant
     browser.open(LINK_LOGIN)
     s(LoginLocators.FIELD_NAME).type("ahahah1@gmail.com")
     s(LoginLocators.FIELD_PASSWORD).type("jk$34_tor")
     s(LoginLocators.BUTTON_SUBMIT).click()
     browser.open(LINK_SPORT)
-    # s(PerformanceSportswear.IMAGE_2).should(be.visible).perform(command.js.scroll_into_view)
     s(PerformanceSportswear.IMAGE_2).should(be.visible).hover()
     s(PerformanceSportswear.BUTTON_ADD_ITEM2).should(be.clickable).click()
     s(PerformanceSportswear.SUCCESS_MESSAGE).should(have.no.text(PerformanceSportswear.TEXT_SUCCESS_MESSAGE))
 
 
+@allure.link("https://trello.com/c/WjUokO7r")
 def test_006_008_003_color_and_size_can_be_checked():
     browser.open(LINK_LOGIN)
     s(LoginLocators.FIELD_NAME).type("ahahah1@gmail.com")
@@ -59,6 +63,7 @@ def test_006_008_003_color_and_size_can_be_checked():
     assert len(selected) == 2
 
 
+@allure.link("https://trello.com/c/dYQgmbfJ")
 def test_006_008_004_add_to_cart_from_product_page_without_color_and_size():
     browser.open(LINK_LOGIN)
     s(LoginLocators.FIELD_NAME).type("ahahah1@gmail.com")
