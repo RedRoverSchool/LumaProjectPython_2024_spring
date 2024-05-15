9c8b979d9c2e9e6a77eb403c7d3efa7eca739302
Svetlana Pulucciu <svetlana.pulucciu250693@gmail.com>
2024-05-15 10:36:35 +0300

    update

--- pages/performans_new_page.py
+++ pages/performans_new_page.py
@@ -1,4 +1,4 @@
-from selene import browser, be, have
+from selene import browser, be, have, Element
 from selene.support.shared.jquery_style import s, ss
 
 url = "https://magento.softwaretestingboard.com/collections/performance-new.html"
@@ -12,5 +12,11 @@ def items_count():
     return len(ss('.product-item-info'))
 
 
-
-
+def product_card_buttons():
+    global product_card
+    product_cards = ss('.product-image-container')
+    for i, product_card in enumerate(product_cards[:5]):
+        product_card.hover()
+    add_to_cart_button = product_card.s(".actions-primary")
+    add_to_wishlist_button = product_card.s(".actions-secondary")
+    add_to_comparison_button = product_card.s(".action.tocompare")
--- tests/test_performance_sportswear_new.py
+++ tests/test_performance_sportswear_new.py
@@ -8,3 +8,13 @@ from pages import performans_new_page
 def test_check_count_of_products(login):
     performans_new_page.visit()
     assert performans_new_page.items_count() == 5
+
+@allure.feature("What's new > Performance Sportswear> NewEach product card contains buttons for adding to cart, adding to wishlist and adding to comparison list")
+@allure.link("https://trello.com/c/YuNxu4x4")
+def test_product_card_buttons(login, product_card_button:
+    performans_new_page.visit()
+    add_to_cart_button.should(be.visible)
+    add_to_wishlist_button.should(be.visible)
+    add_to_comparison_button.should(be.visible)
+
+
