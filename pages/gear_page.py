ef0cc8843ef17ad130f227f05a9fc4271989d2d6
Cruz06 <Olarut72@gmail.com>
2024-05-18 18:24:40 +0300

    TC_009.005.004 | Gear catalog > Fitness Equipment > Set of Sprite Yoga Straps > Put set or a few sets of “Sprite Yoga Strap 8 foot” in the cart and repair test 6.8.4

--- pages/gear_page.py
+++ pages/gear_page.py
@@ -1,5 +1,7 @@
 from data.links import GEAR_PAGE_URL
 from pages.base_page import BasePage
+from selene import browser, have
+
 
 class GearPage(BasePage):
     def __init__(self, browser):
@@ -8,3 +10,9 @@ class GearPage(BasePage):
 
     def open_page(self):
         self.visit(GEAR_PAGE_URL)
+
+
+def visit():
+    browser.open(GEAR_PAGE_URL)
+
+
