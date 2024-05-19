ed6856c3591b2fa80a65db4af25c80f175d3b572
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-05-19 16:14:09 +0300

    TC_011.012.005 | Sale > Men’s Deals > Check card arrangement according to chosen option

--- data/page_data.py
+++ data/page_data.py
@@ -26,6 +26,7 @@ class WishListData:
 class MenSalePageData:
     breadcrumbs_path = ['Home', 'Men Sale']
     page_title = "Men Sale"
+    display_options = ["grid", "list"]
 
     
 class MenPageData:
--- tests/test_men_sale_page.py
+++ tests/test_men_sale_page.py
@@ -5,6 +5,7 @@ from pages.men_sale_page import MenSalePage
 from selene import browser
 from data.page_data import MenSalePageData as data
 
+
 @allure.link('https://trello.com/c/j98xpncK/402-tc011012001-sale-mens-deals-verify-bread-crumbs-display')
 @allure.title('Verify Bread Crumbs display')
 def test_verify_bread_crumbs_display():
@@ -20,6 +21,7 @@ def test_verify_page_title():
     page = MenSalePage(browser)
     page.is_page_title_present()
     page.is_page_title_correct()
+
 
 @allure.link("https://trello.com/c/wnMvuIUl")
 @allure.title("Verify total number of items on the page")
@@ -37,3 +39,14 @@ def test_verify_only_cards_with_men_products_are_present():
     page = MenSalePage(browser)
     page.is_product_list_present()
     page.are_only_product_cards_for_men_present()
+
+
+@allure.link("https://trello.com/c/XCyyXog8")
+@allure.title("Check card arrangement according to chosen option")
+def test_check_card_arrangement():
+    page = MenSalePage(browser)
+    page.is_product_list_present()
+    page.check_selected_view_option(data.display_options[0])
+    page.check_products_in_list_arrangement(data.display_options[0])
+    page.switch_to_display_option(data.display_options[1])
+    page.check_products_in_list_arrangement(data.display_options[1])
