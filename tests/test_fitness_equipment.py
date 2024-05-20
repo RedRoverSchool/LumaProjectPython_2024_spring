61ba88a1a996df5e386b1fbc356b3ccb09daf1fd
Andkorz <opttorgportal@gmail.com>
2024-05-20 21:02:45 +0200

    TC_013.001.004 | Compare products | From any catalog's page > Verify after clicking on the compare button user is redirected to the Compare Products page.

--- tests/test_fitness_equipment.py
+++ tests/test_fitness_equipment.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import time
-
->>>>>>> main
 import allure
 from selene import browser
 from pages.set_of_sprite_yoga_straps_page import SetYogaStraps
@@ -12,15 +7,12 @@ from data.links import SET_YOGA_STRAPS_URL
 from pages import set_of_sprite_yoga_straps_page
 import pytest
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 @allure.suite("US_009.005 | Gear catalog > Fitness Equipment > Set of Sprite Yoga Straps")
 class TestFitnessEquipment:
     @allure.link("https://trello.com/c/sLFXvIMH")
-    @allure.title("TC_009.005.003| Gear catalog > Fitness Equipment > Set of Sprite Yoga Straps >" 
-                    "Adding more than the available quantity \"Sprite Yoga Strap 6 foot\" to Shopping Cart")
+    @allure.title("TC_009.005.003| Gear catalog > Fitness Equipment > Set of Sprite Yoga Straps >"
+                  "Adding more than the available quantity \"Sprite Yoga Strap 6 foot\" to Shopping Cart")
     def test_adding_more_than_the_available_quantity(self, browser_management):
         page = SetYogaStraps(browser)
         page.visit(SET_YOGA_STRAPS_URL)
@@ -39,4 +31,3 @@ def test_009_005_004_put_sets_of_straps_in_the_cart():
     set_of_sprite_yoga_straps_page.add_to_cart_set_8_foot(3)
     set_of_sprite_yoga_straps_page.is_visible_success_message()
     set_of_sprite_yoga_straps_page.check_nr_of_items_in_cart(4)
-
