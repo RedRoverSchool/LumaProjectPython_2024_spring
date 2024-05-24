9ba5d4db471a121f433d49824dab22ed1c773a96
Svetlana Pulucciu <svetlana.pulucciu250693@gmail.com>
2024-05-24 11:58:58 +0300

    modified:   pages/product_page.py Please enter the commit message for your changes. Lines starting

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,8 +3,5 @@ from selene.support.conditions import be
 from selene.support.shared.jquery_style import s
 
 
-
-
-
 def assert_reviews_title_is_visible():
     s("#tab-label-reviews-title").should(be.visible)
